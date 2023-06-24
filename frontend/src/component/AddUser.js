8e311b3b22d74038a6efdf81f6dfa7a0832b7ccd
hibhaqqi <haqqi09@gmail.com>
2023-06-24 21:06:53 +0800

    error value to db

--- frontend/src/component/AddUser.js
+++ frontend/src/component/AddUser.js
@@ -1,20 +1,51 @@
-import React from 'react'
+import React, {useState} from 'react'
+import axios from 'axios'
+import{useNavigate} from "react-router-dom"
 
 const AddUser = () => {
-  return (
+const [name, setName] = useState("");
+const [email, setEmail] = useState("");
+const [gender, setGender] = useState("Male");
+const navigate =useNavigate();
+
+
+const saveUser =async (e) =>{
+    e.preventDefault();
+    try {
+        await axios.post("http://localhost:5000/users",(
+            name,
+            email,
+            gender
+        ));
+        navigate("/");
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+return (
     <div className="column mt-5 is-centered">
         <div className="column is-half">
-            <form action="">
+            <form onSubmit={saveUser}>
                 <div className="field">
                     <label  className="label">Name</label>
                     <div className="control">
-                        <input type="text" className="input" placeholder='Nama' />
+                        <input 
+                        type="text" 
+                        className="input" 
+                        value={name} 
+                        onChange ={(e) => setName(e.target.value)}
+                        placeholder="name" />
                     </div>
                 </div>
                 <div className="field">
                     <label  className="label">Email</label>
                     <div className="control">
-                        <input type="text" className="input" placeholder='email' />
+                        <input 
+                        type="text" className="input" 
+                        value={email} 
+                        onChange ={(e) => setEmail(e.target.value)}
+                        placeholder="email" />
                     </div>
                 </div>
                 
@@ -22,7 +53,9 @@ const AddUser = () => {
                     <label  className="label">Gender</label>
                     <div className="control">
                         <div className="select is-fullwidth">
-                            <select>
+                            <select 
+                            value={gender} 
+                            onChange ={(e) => setGender(e.target.value)}>
                                 <option value="Male">Male</option>
                                 <option value="Female">Female</option>
                             </select>
