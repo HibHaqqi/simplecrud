4cc56cb3daacea9f9403b92444f476a701ed1fbc
hibhaqqi <haqqi09@gmail.com>
2023-06-26 19:44:01 +0800

    add form edituser

---
+++ frontend/src/component/EditUser.js
@@ -0,0 +1,76 @@
+import React, {useState} from 'react';
+import axios from 'axios';
+import{useNavigate} from "react-router-dom";
+
+const EditUser = () => {
+const [name, setName] = useState("");
+const [email, setEmail] = useState("");
+const [gender, setGender] = useState("Male");
+const navigate =useNavigate();
+
+
+const saveUser =async (e) =>{
+    e.preventDefault();
+    try {
+        await axios.post("http://localhost:5000/users",{
+            name,
+            email,
+            gender
+    });
+        navigate("/");
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+return (
+    <div className="column mt-5 is-centered">
+        <div className="column is-half">
+            <form onSubmit={saveUser}>
+                <div className="field">
+                    <label  className="name">Name</label>
+                    <div className="control">
+                        <input 
+                        type="text" 
+                        className="input" 
+                        value={name} 
+                        onChange ={(e) => setName(e.target.value)}
+                        placeholder="name" />
+                        
+                    </div>
+                </div>
+                <div className="field">
+                    <label  className="email">Email</label>
+                    <div className="control">
+                        <input 
+                        type="text" className="input" 
+                        value={email} 
+                        onChange ={(e) => setEmail(e.target.value)}
+                        placeholder="email" />
+                    </div>
+                </div>
+                
+                <div className="field">
+                    <label  className="gender">Gender</label>
+                    <div className="control">
+                        <div className="select is-fullwidth">
+                            <select 
+                            value={gender} 
+                            onChange ={(e) => setGender(e.target.value)}>
+                                <option value="Male">Male</option>
+                                <option value="Female">Female</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+                <div className="field">
+                    <button type='submit' className='button is-success'>Update</button>
+                </div>
+            </form>
+        </div>
+
+    </div>
+  )
+}
+
+export default EditUser;
